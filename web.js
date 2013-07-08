f37717dc2f89129a6ad5c7319effcddbfe53f686
jorju <jorju2004@gmail.com>
2013-07-08 13:55:15 +0000

    fixed

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send(
-      buf2string(fs.readFile('index.html' ) )
+      fs.readFile('index.html' ).toString() 
                )
 });
 
